0634ccdb676c115683ec31d05c418301eeedffaf
ahmadbillitwiscode <ahmad_billi@twiscoder.com>
2021-02-17 01:53:43 +0700

    1.0.1

---
+++ Sources/CoreOfflineAppTestKit/Util/ApiUrl.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ahmad Billi Afandi on 16/02/21.
+//
+
+import Foundation
+
+public class ApiUrl {
+    public static let baseUrl = "https://rawg.io/api/"
+    public static let visitUrl = "https://rawg.io/games/"
+}
